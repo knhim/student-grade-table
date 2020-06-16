24bbb53ef6c6292746c36ff10806114b129f1c32
knhim <dev@KEVIN-LAPTOP.localdomain>
2020-06-16 15:32:29 -0700

    instantiated the app class and called the start method

--- components/app.js
+++ components/app.js
@@ -7,4 +7,24 @@ class App {
     console.log(grades);
   }
 
+  constructor() {
+    this.handleGetGradesSuccess.bind(this);
+    this.handleGetGradesError.bind(this);
+  }
+
+  getGrades() {
+
+    return $.ajax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      headers: {"x-access-token":"amdTkuJE"},
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError
+    })
+  }
+
+  start() {
+    this.getGrades();
+  }
+
 }
