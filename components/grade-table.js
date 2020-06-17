10cc56214e3c9d3ebc97e8f35b613cc098c2914d
Kevin <nhimkap4@gmail.com>
2020-06-17 15:09:12 -0700

    added a second parameter noGradesElement to the GradeTable constructor

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,47 +1,58 @@
 class GradeTable {
-  constructor(tableElement) {
+  constructor(tableElement, noGradesElement) {
     this.tableElement = tableElement;
+    this.noGradesElement = noGradesElement;
   }
 
   updateGrades(grades) {
-    var tbodyElement = this.tableElement.querySelector('tbody');
-    tbodyElement.textContent = '';
+    // var tbodyElement = this.tableElement.querySelector('tbody');
+    // tbodyElement.textContent = '';
+    // for (var i = 0; i < grades.length; i++) {
+    //   var trElement = document.createElement('tr');
+    //   var tdName = document.createElement('td');
+    //   var tdCourseName = document.createElement('td');
+    //   var tdStudentGrade = document.createElement('td');
+
+    //   tdName.textContent = grades[i].name;
+    //   tdCourseName.textContent = grades[i].course;
+    //   tdStudentGrade.textContent = grades[i].grade;
+
+    //   trElement.append(tdName, tdCourseName, tdStudentGrade);
+    //   tbodyElement.append(trElement);
+    // }
     for (var i = 0; i < grades.length; i++) {
-      var trElement = document.createElement('tr');
-      var tdName = document.createElement('td');
-      var tdCourseName = document.createElement('td');
-      var tdStudentGrade = document.createElement('td');
-
-      tdName.textContent = grades[i].name;
-      tdCourseName.textContent = grades[i].course;
-      tdStudentGrade.textContent = grades[i].grade;
-
-      trElement.append(tdName, tdCourseName, tdStudentGrade);
-      tbodyElement.append(trElement);
+      this.renderGradeRow(grades[i], this.deleteGrade(grades));
     }
+
   }
 
   renderGradeRow(data, deleteGrade) {
-    var tbodyElement = this.tableElement.querySelector('tbody');
-
-    console.log(deleteGrade);
-    for (var i = 0; i < data.length; i++) {
-      var trElement = document.createElement('tr')
-      var tdName = document.createElement('td');
-      var tdCourseName = document.createElement('td');
-      var tdStudentGrade = document.createElement('td');
-
-      tdName.textContent = data[i].name;
-      tdCourseName.textContent = data[i].course;
-      tdStudentGrade.textContent = data[i].grade;
-    }
-    //create Delete button and add an event listener
+    var trElement = document.createElement('tr')
+    var tdName = document.createElement('td');
+    var tdCourseName = document.createElement('td');
+    var tdStudentGrade = document.createElement('td');
+    var tdDelete = document.createElement('td');
     var tdDeleteButton = document.createElement('button');
+
+    tdName.textContent = data.name;
+    tdCourseName.textContent = data.course;
+    tdStudentGrade.textContent = data.grade;
+
+    trElement.append(tdName, tdCourseName, tdStudentGrade);
+    this.tableElement.lastElementChild.append(trElement);
+
     tdDeleteButton.textContent = "DELETE";
-    tdDeleteButton.addEventListener('click', deleteGrade(data.id));
-    //return the created table row element
+    tdDeleteButton.addEventListener('click', function () {
+      deleteGrade(data.id);
+    });
+
+
     return trElement;
 
+  }
+
+  onDeleteClick(deleteGrade) {
+    this.deleteGrade = deleteGrade;
   }
 
   deleteGrade(id) {
