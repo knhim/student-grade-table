badf7b04abd514520b8d6fbdefb8a37abb98a05b
Kevin <nhimkap4@gmail.com>
2020-06-17 13:14:57 -0700

    defined a new method named renderGradeRow

--- components/grade-table.js
+++ components/grade-table.js
@@ -19,16 +19,33 @@ class GradeTable {
       trElement.append(tdName, tdCourseName, tdStudentGrade);
       tbodyElement.append(trElement);
     }
+  }
 
+  renderGradeRow(data, deleteGrade) {
+    var tbodyElement = this.tableElement.querySelector('tbody');
 
+    console.log(deleteGrade);
+    for (var i = 0; i < data.length; i++) {
+      var trElement = document.createElement('tr')
+      var tdName = document.createElement('td');
+      var tdCourseName = document.createElement('td');
+      var tdStudentGrade = document.createElement('td');
+
+      tdName.textContent = data[i].name;
+      tdCourseName.textContent = data[i].course;
+      tdStudentGrade.textContent = data[i].grade;
+    }
+    //create Delete button and add an event listener
+    var tdDeleteButton = document.createElement('button');
+    tdDeleteButton.textContent = "DELETE";
+    tdDeleteButton.addEventListener('click', deleteGrade(data.id));
+    //return the created table row element
+    return trElement;
 
   }
 
-
-
-
-
-
-
+  deleteGrade(id) {
+    console.log(id);
+  }
 
 }
