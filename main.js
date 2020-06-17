8c33ad6d7fc24185a6a06695e5f31eb0a0fd8a0d
Kevin <nhimkap4@gmail.com>
2020-06-16 17:11:25 -0700

    instantiated PageHeader object, and passed in the header element as an argument

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 var tableEle = document.querySelector('table');
 var gradeTable = new GradeTable(tableEle);
 
+//set header element to a variable , and created a new pageheader object w/headerEle as an argument
+var headerEle = document.querySelector('header');
+var pageHeader = new PageHeader(headerEle);
+
+
+
+
 var app = new App(gradeTable);
 app.start();
